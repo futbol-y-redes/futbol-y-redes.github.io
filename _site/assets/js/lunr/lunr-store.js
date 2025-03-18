53b072f6aecb21e5f638a1ecefca52049360c48a
futbol-y-redes <futbolyredes@gmail.com>
2025-03-18 12:22:37 +0100

    v1.1.4 - mint

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,16 +1,4 @@
-var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/2025/03/18/welcome-to-jekyll.html",
-        "teaser": null
-      },{
-    "title": "About",
-    "excerpt":"This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at jekyllrb.com You can find the source code for Minima at GitHub: jekyll / minima You can find the source code for Jekyll at GitHub: jekyll...","url": "http://localhost:4000/about/"
-  },{
-    "title": "About",
-    "excerpt":"This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at jekyllrb.com You can find the source code for Minima at GitHub: jekyll / minima You can find the source code for Jekyll at GitHub: jekyll...","url": "http://localhost:4000/about/"
-  },{
+var store = [,{
     "title": "Contacto",
     "excerpt":"Nos encanta compartir ideas. Hablar de sistemas complejos. De redes. Pero, sobre todo, de fútbol. No dudes en contactar con nosotros si tienes inquietudes sobre todos estos temas. Complex Systems &amp; Sports Analytics       Laboratorio de Redes Biológicas       Centro de Tecnología Biomédica (UPM)       Campus de Montegancedo       28223 Pozuelo de Alarcón (España)...","url": "http://localhost:4000/contacto/"
   },{
